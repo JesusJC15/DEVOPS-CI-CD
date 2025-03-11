9476c87e51bc35ae5bccde28dfb5a2b4a3e1dad8
Natalia <natalia.espitia-e@mail.escuelaing.edu.co>
2025-03-11 17:09:55 -0500

    Laboratorio 4 añadido

---
+++ src/main/java/edu/eci/cvds/ecireserves/EciReservesApplication.java
@@ -0,0 +1,16 @@
+package edu.eci.cvds.ecireserves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.Generated;
+
+@SpringBootApplication
+@Generated
+public class EciReservesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EciReservesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/cvds/ecireserves/EciReservesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.ecireserves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EciReservesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
